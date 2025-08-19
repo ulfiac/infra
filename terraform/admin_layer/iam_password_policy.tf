eeebfd7a3344f5eb6660e5212451b0fc27f1ecff
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-08-18 21:49:45 -0400

    add iam password policy

---
+++ terraform/admin_layer/iam_password_policy.tf
@@ -0,0 +1,12 @@
+#trivy:ignore:AVD-AWS-0062 (MEDIUM): Password policy allows a maximum password age of greater than 90 days.
+resource "aws_iam_account_password_policy" "password_policy" {
+  allow_users_to_change_password = true
+  hard_expiry                    = false
+  max_password_age               = 180
+  minimum_password_length        = 42
+  password_reuse_prevention      = 10
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_symbols                = true
+  require_uppercase_characters   = true
+}
