42b2e8dd449b8d35d36cb1a5548847d6adc76a8d
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-08-24 13:00:08 -0400

    add cloudtrail logging

--- terraform/admin_layer/main.tf
+++ terraform/admin_layer/main.tf
@@ -1,10 +1,19 @@
 locals {
+  account_id = data.aws_caller_identity.current.account_id
 
-  iam_group_name             = "default-group"
-  iam_policy_name_mfa        = "enforce-mfa"
-  iam_policy_name_roleswitch = "role-switch"
-  iam_role_name_admin        = "role-switch-admin"
-  iam_role_name_poweruser    = "role-switch-power-user"
-  iam_user_name              = "actual"
+  cloudtrail_name          = "multi-region-trail"
+  cloudtrail_s3_key_prefix = "cloudtrail"
 
+  cloudwatch_log_group_name = "/aws/cloudtrail/${local.cloudtrail_name}"
+
+  iam_group_name                           = "default-group"
+  iam_policy_name_cloudtrail_to_cloudwatch = "cloudtrail-to-cloudwatch"
+  iam_policy_name_mfa                      = "enforce-mfa"
+  iam_policy_name_roleswitch               = "role-switch"
+  iam_role_name_admin                      = "role-switch-admin"
+  iam_role_name_cloudtrail_to_cloudwatch   = "cloudtrail-to-cloudwatch"
+  iam_role_name_poweruser                  = "role-switch-power-user"
+  iam_user_name                            = "actual"
+
+  s3_bucket_name_logging = "logging-${var.unique_name}"
 }
--- terraform/admin_layer/variables.tf
+++ terraform/admin_layer/variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   description = "The AWS region to deploy resources in."
   type        = string
 }
+
+variable "unique_name" {
+  description = "String the can be added to names that must be unique (eg s3 buckets, etc)."
+  type        = string
+}
