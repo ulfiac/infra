73be806290ad78420b5e32b33a388a5560459e4d
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-08-27 21:08:05 -0400

    adjust the iam role cloudtrail assumes to write to cloudwatch

--- terraform/admin_layer/main.tf
+++ terraform/admin_layer/main.tf
@@ -1,5 +1,6 @@
 locals {
   account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.region
 
   cloudtrail_name          = "multi-region-trail"
   cloudtrail_s3_key_prefix = "cloudtrail"
