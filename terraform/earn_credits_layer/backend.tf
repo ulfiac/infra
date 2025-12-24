1c3cd7a1e15e4367649f8bb79d8a69bb8565034a
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-12-24 18:38:54 -0500

    remove earn credits layer

--- terraform/earn_credits_layer/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-state-ulfiac"
-    encrypt      = true
-    key          = "aws-infrastructure/earn_credits_layer/us-east-2/terraform.tfstate"
-    region       = "us-east-2"
-    use_lockfile = true
-  }
-}
