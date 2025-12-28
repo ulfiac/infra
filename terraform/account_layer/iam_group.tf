e752eff72ee9c134657a66d6fbdbb3011dc2be0b
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-12-28 13:38:57 -0500

    remove user from account layer

--- terraform/account_layer/iam_group.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_iam_group" "default_group" {
-  name = local.iam_group_name
-}
-
-resource "aws_iam_group_policy_attachment" "read_only_access" {
-  group      = aws_iam_group.default_group.name
-  policy_arn = data.aws_iam_policy.read_only_access.arn
-}
