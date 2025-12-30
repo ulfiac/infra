869d64361bc4fc5f550fdc75f8c179a90c999004
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-12-29 21:47:19 -0500

    remove logs from account layer

--- terraform/account_layer/cloudtrail.tf
+++ terraform/account_layer/cloudtrail.tf
@@ -1,17 +1,17 @@
-resource "aws_cloudtrail" "multi_region_trail" {
-  name = local.cloudtrail_name
+# resource "aws_cloudtrail" "multi_region_trail" {
+#   name = local.cloudtrail_name
 
-  enable_log_file_validation    = true
-  enable_logging                = true
-  include_global_service_events = true
-  is_multi_region_trail         = true
-  kms_key_id                    = aws_kms_key.logs.arn
+#   enable_log_file_validation    = true
+#   enable_logging                = true
+#   include_global_service_events = true
+#   is_multi_region_trail         = true
+#   kms_key_id                    = aws_kms_key.logs.arn
 
-  s3_bucket_name = aws_s3_bucket.logs.bucket
-  s3_key_prefix  = local.s3_key_prefix_cloudtrail
+#   s3_bucket_name = aws_s3_bucket.logs.bucket
+#   s3_key_prefix  = local.s3_key_prefix_cloudtrail
 
-  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
-  cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_to_cloudwatch_role.arn
+#   cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
+#   cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_to_cloudwatch_role.arn
 
-  depends_on = [aws_s3_bucket_policy.logs]
-}
+#   depends_on = [aws_s3_bucket_policy.logs]
+# }
--- terraform/account_layer/cloudwatch.tf
+++ terraform/account_layer/cloudwatch.tf
@@ -1,5 +1,5 @@
-resource "aws_cloudwatch_log_group" "cloudtrail" {
-  name              = local.cloudwatch_log_group_name
-  retention_in_days = 30
-  kms_key_id        = aws_kms_key.logs.arn
-}
+# resource "aws_cloudwatch_log_group" "cloudtrail" {
+#   name              = local.cloudwatch_log_group_name
+#   retention_in_days = 30
+#   kms_key_id        = aws_kms_key.logs.arn
+# }
