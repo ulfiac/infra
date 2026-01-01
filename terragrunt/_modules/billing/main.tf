49e4ef7c8670d2d5813b581666e61ad088b8134c
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-01-01 14:02:44 -0500

    exclude credits and add notifications on actual

--- terragrunt/_modules/billing/main.tf
+++ terragrunt/_modules/billing/main.tf
@@ -1,9 +1,10 @@
 locals {
-  aws_account_id            = data.aws_caller_identity.current.account_id
-  aws_region                = data.aws_region.current.region
-  billing_budget_name       = "monthly-budget"
-  billing_budget_thresholds = [20, 40, 60, 80, 100]
-  sns_topic_name            = "monthly-budget"
+  aws_account_id               = data.aws_caller_identity.current.account_id
+  aws_region                   = data.aws_region.current.region
+  budget_name                  = "monthly-budget"
+  budget_thresholds_actual     = [20, 40, 60, 80, 100]
+  budget_thresholds_forecasted = [100, 150, 200]
+  sns_topic_name               = "monthly-budget"
 }
 
 #trivy:ignore:AVD-AWS-0095 (HIGH): Topic does not have encryption enabled.
@@ -19,14 +20,29 @@ resource "aws_sns_topic_subscription" "monthly_budget" {
 }
 
 resource "aws_budgets_budget" "monthly_budget" {
-  name         = local.billing_budget_name
+  name         = local.budget_name
   budget_type  = "COST"
   limit_amount = "1"
   limit_unit   = "USD"
   time_unit    = "MONTHLY"
 
+  cost_types {
+    include_credit = false
+  }
+
   dynamic "notification" {
-    for_each = local.billing_budget_thresholds
+    for_each = local.budget_thresholds_actual
+    content {
+      comparison_operator       = "GREATER_THAN"
+      threshold                 = notification.value
+      threshold_type            = "PERCENTAGE"
+      notification_type         = "ACTUAL"
+      subscriber_sns_topic_arns = [aws_sns_topic.monthly_budget.arn]
+    }
+  }
+
+  dynamic "notification" {
+    for_each = local.budget_thresholds_forecasted
     content {
       comparison_operator       = "GREATER_THAN"
       threshold                 = notification.value
