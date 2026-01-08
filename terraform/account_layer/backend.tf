99cdf05e4a808e7c3632f48f3eb48383c8075e2d
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-01-07 20:00:55 -0500

    remove account layer

--- terraform/account_layer/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-state-ulfiac"
-    encrypt      = true
-    key          = "aws-infrastructure/account_layer/us-east-2/terraform.tfstate"
-    region       = "us-east-2"
-    use_lockfile = true
-  }
-}
